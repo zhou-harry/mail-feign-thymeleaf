23f0f739bd426897c9183d33138d7a9ebd12f13b
zhouhong <hzhou@tplhk.cntaiping.com>
2019-12-19 11:20:21 +0800

    commit

--- mail-api/src/main/java/com/harry/api/config/FeignClientConfig.java
+++ mail-api/src/main/java/com/harry/api/config/FeignClientConfig.java
@@ -1,9 +1,13 @@
 package com.harry.api.config;
 
+import com.harry.api.interceptor.FeignBasicAuthRequestInterceptor;
 import feign.Contract;
 import feign.auth.BasicAuthRequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
 
 /**
  * @author zhouhong
@@ -15,10 +19,13 @@ import org.springframework.context.annotation.Configuration;
 //@Configuration
 public class FeignClientConfig {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     /**
      * Contract feign的默认契约
      * 在FeignClient中的接口中使用Feign自带的注解
      * 例如：@RequestLine("GET /eureka/apps/{serviceName}")
+     *
      * @return
      */
 //    @Bean
@@ -26,13 +33,21 @@ public class FeignClientConfig {
 //        return new Contract.Default();
 //    }
 
+//    @Bean
+//    public RequestContextListener requestContextListener() {
+//        logger.info("init listener...");
+//        return new RequestContextListener();
+//    }
+
     /**
      * 请求接口中要进行基于Http Basic的认证后才能调用
+     *
      * @return
      */
     @Bean
-    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        return new BasicAuthRequestInterceptor("user", "password");
+    public FeignBasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+//        return new BasicAuthRequestInterceptor("user", "password");
+        return new FeignBasicAuthRequestInterceptor();
     }
 
 }
---
+++ mail-api/src/main/java/com/harry/api/interceptor/FeignBasicAuthRequestInterceptor.java
@@ -0,0 +1,46 @@
+package com.harry.api.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author zhouhong
+ * @version 1.0
+ * @title: FeignBasicAuthRequestInterceptor
+ * @description: TODO
+ * @date 2019/10/15 18:19
+ */
+public class FeignBasicAuthRequestInterceptor implements RequestInterceptor {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    public FeignBasicAuthRequestInterceptor() {
+        logger.info("====init interceptor");
+    }
+
+    /**
+     * Feign不开启Hystrix支持时:RequestContextHolder.currentRequestAttributes()才能拿到内容<br/>
+     * Feign开启Hystrix支持时:RequestContextHolder.currentRequestAttributes()返回null<br/>
+     * 原因在于，Hystrix的默认隔离策略是THREAD 。而 RequestContextHolder 源码中，使用了两个血淋淋的ThreadLocal 。
+     * 解决方案见有道云笔记spring篇：《Hystrix传播ThreadLocal对象（两种方案）》
+     *
+     * @param requestTemplate
+     */
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+        HttpServletRequest request = attributes.getRequest();
+
+        logger.info("====token:", request.getHeader("Access-Token"));
+
+        //添加token
+        requestTemplate.header("Access-Token", request.getHeader("Access-Token"));
+    }
+}
